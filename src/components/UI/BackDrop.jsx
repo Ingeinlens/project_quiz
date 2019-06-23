627c7c0dbf1d2a9635daaccfbc8ecfb9b4310d59
Ingeinlens <ingeinlens@gmai.com>
2019-06-23 14:38:15 +0800

    Настройка работы нашего приложения для работы с сервером

--- src/components/UI/BackDrop.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const BackDrop = props => <div className="BackDrop" onClick={props.onClick}></div>
-
-export default BackDrop;
--- src/js/index.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom';
-import App from '../components/App';
-import '../scss/index.scss';
-
-ReactDOM.render(<BrowserRouter><App/></BrowserRouter>, document.getElementById('root'))
