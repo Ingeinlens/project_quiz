393f0d5c91469f817c92c4de6e35fe86b8afaadb
Ingeinlens <ingeinlens@gmai.com>
2019-06-24 16:33:37 +0800

    Перевод QuizList на redux

--- src/index.js
+++ src/index.js
@@ -4,11 +4,21 @@ import './index.css'
 import App from './App'
 import {BrowserRouter} from 'react-router-dom'
 import registerServiceWorker from './registerServiceWorker'
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import rootReducer from './store/reducers/rootReducer';
+import thunk from 'redux-thunk'
 
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunk)
+)
 const app = (
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 )
 
 ReactDOM.render(app, document.getElementById('root'))
