13148119c33a61dfb2484227f061f05b4eb4bf7e
Ingeinlens <ingeinlens@gmai.com>
2019-06-25 11:09:02 +0800

    Установка компонента Quiz на Redux

--- src/store/reducers/quiz.js
+++ src/store/reducers/quiz.js
@@ -1,8 +1,22 @@
-import {FETCH_QUIZES_ERROR, FETCH_QUIZES_START, FETCH_QUIZES_SUCCESS } from '../actions/actionTypes'
+import {
+    FETCH_QUIZES_ERROR, 
+    FETCH_QUIZES_START, 
+    FETCH_QUIZES_SUCCESS,
+    FETCH_QUIZ_SUCCESS,
+    QUIZ_SET_STATE,
+    FINISH_QUIZ,
+    QUIZ_NEXT_QUESTION,
+    QUIZ_RETRY 
+} from '../actions/actionTypes'
 
 const initialState = {
     quizes: [],
-    loading: false
+    loading: false,
+    results: {},
+    isFinished: false,
+    activeQuestion: 0,
+    answerState: null,
+    quiz: null
 }
 export default function quizReducer(state = initialState, action) {
     switch (action.type) {
@@ -17,6 +31,30 @@ export default function quizReducer(state = initialState, action) {
         case FETCH_QUIZES_ERROR:
             return {
                 ...state, loading: false    
+            }
+        case FETCH_QUIZ_SUCCESS:
+            return {
+                ...state, loading: false, quiz: action.quiz
+            }
+        case QUIZ_SET_STATE: 
+            return {
+                ...state, answerState: action.answerState, results: action.results
+            }
+        case FINISH_QUIZ: 
+            return {
+                ...state, isFinished: true
+            }
+        case QUIZ_NEXT_QUESTION:
+            return {
+                ...state, answerState: null, activeQuestion: action.questionNum
+            }
+        case QUIZ_RETRY: 
+            return {
+                ...state, 
+                activeQuestion: 0,
+                answerState: null,
+                isFinished: false,
+                results: {}
             }
         default:
             return state
